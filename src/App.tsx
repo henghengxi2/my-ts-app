d961041be8965b9c7ebdfe34ad96f5015ff5d17a
weixuyin <xuyin.wei@casstime.com>
2020-08-31 11:08:33 +0800

    test(app.tsx): add a charater for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React, { FC, SFC } from "react";
-import { Button } from "antd";
-import "./App.scss";
+import React, { FC, SFC } from 'react';
+import { Button } from 'antd';
+import './App.scss';
 
 const App: SFC = () => {
   return (
     <div className="form">
-      <div className="form-text">text hhh 1</div>
+      <div className="form-text">text hhh 1 2</div>
       <Button type="dashed">primary</Button>
     </div>
   );
