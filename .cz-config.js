e2096de54e72cddd3b7f538b7b9753c2fc65859d
weixuyin <xuyin.wei@casstime.com>
2020-08-31 14:20:16 +0800

    ci(模块：基础设施): change .cz-config.js

--- .cz-config.js
+++ .cz-config.js
@@ -2,46 +2,28 @@
 
 module.exports = {
   types: [
-    { value: '特性', name: '特性:    一个新的特性' },
-    { value: '修复', name: '修复:    修复一个Bug' },
-    { value: '文档', name: '文档:    变更的只有文档' },
-    { value: '格式', name: '格式:    空格, 分号等格式修复' },
-    { value: '重构', name: '重构:    代码重构，注意和特性、修复区分开' },
-    { value: '性能', name: '性能:    提升性能' },
-    { value: '测试', name: '测试:    添加一个测试' },
-    { value: '工具', name: '工具:    开发工具变动(构建、脚手架工具等)' },
-    { value: '回滚', name: '回滚:    代码回退' },
+    { value: 'feat', name: 'feat:     新增一个功能' },
+    { value: 'fix', name: 'fix:      修复一个Bug' },
+    { value: 'test', name: 'test:     添加一个测试' },
+    { value: 'style', name: 'style:    代码格式、UI（不影响功能，例如空格、分号等格式修正）' },
+    { value: 'docs', name: 'docs:     更新文档、注释' },
+    { value: 'refactor', name: 'refactor: 代码重构' },
+    { value: 'perf', name: 'perf:     改善性能' },
+    { value: 'revert', name: 'revert:   代码回退' },
+    { value: 'ci', name: 'ci:       更改持续集成软件的配置文件和 package 中的 scripts 命令' },
+    { value: 'build', name: 'build:    变更项目构建或外部依赖（例如 webpack、gulp、npm 等）' },
+    { value: 'chore', name: 'chore:    变更构建流程或辅助工具' },
   ],
-
-  scopes: [{ name: 'merge' }, { name: 'style' }, { name: '构建工具' }, { name: '组件' }],
-
-  // it needs to match the value for field type. Eg.: 'fix'
-  /*
-  scopeOverrides: {
-    fix: [
-      {name: 'merge'},
-      {name: 'style'},
-      {name: 'e2eTest'},
-      {name: 'unitTest'}
-    ]
-  },
-  */
-  // override the messages, defaults are as follows
+  scopes: [{ name: '模块：基础设施' }, { name: '模块：行业标准工时' }],
   messages: {
-    type: '选择一种你的提交类型:',
-    scope: '选择一个scope (可选):',
-    // used if allowCustomScopes is true
-    customScope: 'Denote the SCOPE of this change:',
+    type: '选择一种你的提交类型: ',
+    scope: '选择一个模块: ',
+    customScope: '自定义一个模块 (可选): ',
     subject: '短说明:\n',
     body: '长说明，使用"|"换行(可选)：\n',
-    breaking: '非兼容性说明 (可选):\n',
-    footer: '关联关闭的issue，例如：#31, #34(可选):\n',
     confirmCommit: '确定提交说明?',
   },
-
-  allowCustomScopes: true,
-  allowBreakingChanges: ['特性', '修复'],
-
-  // limit subject length
   subjectLimit: 100,
+  allowCustomScopes: true,
+  skipQuestions: ['breaking', 'footer'],
 };
