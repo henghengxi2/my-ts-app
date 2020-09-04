56a9de01b546c070179c166fa2d54e02a20f93f4
weixuyin <xuyin.wei@casstime.com>
2020-09-04 18:04:51 +0800

    chore(模块：基础设施): 增加@sentry/react

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+Sentry.init({
+  dsn: "https://ee4e1da796884dc88b5ec7a5df111c11@o443460.ingest.sentry.io/5417240",
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+  tracesSampleRate: 1.0,
+});
 ReactDOM.render(
   <React.StrictMode>
     <App />
