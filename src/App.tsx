3561b4ed40b5a30bbb5cac64d043926283f8bcab
weixuyin <xuyin.wei@casstime.com>
2020-09-05 18:01:34 +0800

    chore(模块：基础设施): 添加异常事件

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React, { FC, SFC } from 'react';
+import React, { SFC } from 'react';
 import { Button } from 'antd';
 import './App.scss';
 
 const App: SFC = () => {
+  const handleOnClick = () => {
+    throw new Error('throw error');
+  };
+
   return (
     <div className="form">
       <div className="form-text">text hhh 1 2</div>
-      <Button type="dashed">primary</Button>
+      <Button type="dashed" onClick={handleOnClick}>
+        primary
+      </Button>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as Sentry from '@sentry/react'
+import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
 Sentry.init({
-  dsn: "https://ee4e1da796884dc88b5ec7a5df111c11@o443460.ingest.sentry.io/5417240",
-  integrations: [
-    new Integrations.BrowserTracing(),
-  ],
+  dsn: 'https://4f898b6b90b34101b19b804585a8d8e9@o443460.ingest.sentry.io/5417257',
+  integrations: [new Integrations.BrowserTracing()],
   tracesSampleRate: 1.0,
 });
 ReactDOM.render(
