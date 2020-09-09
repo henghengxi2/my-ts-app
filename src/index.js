a47bf5ef340b2ec761e7d88f9a076ca91f9ed307
weixuyin <xuyin.wei@casstime.com>
2020-09-09 10:59:18 +0800

    feat(模块：基础设施): 添加Sentry.ErrorBoundary

--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const { ErrorBoundary } = Sentry;
 Sentry.init({
   dsn: 'https://4f898b6b90b34101b19b804585a8d8e9@o443460.ingest.sentry.io/5417257',
   integrations: [new Integrations.BrowserTracing()],
   tracesSampleRate: 1.0,
 });
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ErrorBoundary fallback={'An error has occured!!!'}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ErrorBoundary>,
   document.getElementById('root')
 );
 
